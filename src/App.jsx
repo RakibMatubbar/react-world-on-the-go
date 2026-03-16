3329fed052b2887aebcc546a5f1badb4c3dd9f83
RakibMatubbar <rakibmatubbar996@gmail.com>
2026-03-16 13:02:25 +0600

    Data fetch from API & load data | Show flags/name/population

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 
+import { Suspense } from 'react'
 import './App.css'
 
 import Countries from './components/countries/countries'
 
-function App() {
+// Create a promise to get API's Data:
+const countiresPromise = fetch('https://openapi.programming-hero.com/api/all')
+  .then(res => res.json())
 
+function App() {
 
   return (
     <>
-      <Countries></Countries>
+
+      {/* Suspense is a default from React to Show the API's Data into any component: */}
+      <Suspense fallback={<p>Countires Loading...</p>}>
+
+        {/* Countries function called here: and show within Contries func the API's data: */}
+        <Countries countiresPromise={countiresPromise}></Countries>
+
+      </Suspense>
+      
     </>
   )
 }
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,10 +1,25 @@
 
-import React from 'react';
+import React, { use } from 'react';
 
-const Countries = () => {
+import Country from '../Country/Country';
+
+// Destucturing to show the variable's data:
+const Countries = ({countiresPromise}) => {
+
+    // Use (use()) and store the variable into ().
+    const countriesData = use(countiresPromise);
+    const countries = countriesData.countries;
+    // console.log(countries);
+    
+
     return (
         <div>
-            <h1>World On The Go</h1>
+            <h1>World On The Go: {countries.length} </h1>
+
+            {/* Mapping in js and single data store in country, it sent County.jsx */}
+            {
+                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+            }
         </div>
     );
 };
